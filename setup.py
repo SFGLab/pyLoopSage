4748f0a4ec46e03ec554a3ce732a26be6bdf51fb
Sebastian Korsak <49608786+BlackPianoCat@users.noreply.github.com>
2024-11-12 10:15:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='pyLoopSage',  # Package name
-    version='1.0.14',  # Version of the software
+    version='1.0.15',  # Version of the software
     description='An energy-based stochastic model of loop extrusion in chromatin.',
     long_description=long_description,
     long_description_content_type='text/markdown',
