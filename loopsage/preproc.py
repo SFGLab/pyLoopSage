b9a823df2680bf0094d2c4a9fcdadf727ee7b8a5
Sebastian Korsak <49608786+BlackPianoCat@users.noreply.github.com>
2024-11-12 09:45:45 +0100

    Update preproc.py

--- loopsage/preproc.py
+++ loopsage/preproc.py
@@ -2,8 +2,7 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import pyBigWig
-import os
+# import pyBigWig
 from matplotlib.pyplot import figure
 
 def binding_vectors_from_bedpe(bedpe_file,N_beads,region,chrom,normalization=False,viz=False):
@@ -30,16 +29,35 @@ def binding_vectors_from_bedpe(bedpe_file,N_beads,region,chrom,normalization=Fal
     resolution = (region[1]-region[0])//N_beads
     df[1], df[2], df[4], df[5] = (df[1]-region[0])//resolution, (df[2]-region[0])//resolution, (df[4]-region[0])//resolution, (df[5]-region[0])//resolution
     
+    # Check if columns 7 and 8 exist
+    has_col_7_8 = df.shape[1] > 8
+    if has_col_7_8:
+        print("The input file contains CTCF orientation! It will run taking it into account.")
+    else:
+        print("WARNING: The script does not contain CTCF orientation and thus it is not taken into account. If you would like to add CTCF orientation check the documentation.")
+
     # Compute the matrix
     distances = list()
-    L, R = np.zeros(N_beads),np.zeros(N_beads)
+    J = np.zeros((N_beads,N_beads), dtype=np.float64)
+    L, R = np.zeros(N_beads, dtype=np.float64),np.zeros(N_beads, dtype=np.float64)
     for i in range(len(df)):
         x, y = (df[1][i]+df[2][i])//2, (df[4][i]+df[5][i])//2
         distances.append(distance_point_line(x,y))
-        if df[7][i]>=0: L[x] += df[6][i]*(1-df[7][i])
-        if df[8][i]>=0: L[y] += df[6][i]*(1-df[8][i])
-        if df[7][i]>=0: R[x] += df[6][i]*df[7][i]
-        if df[8][i]>=0: R[y] += df[6][i]*df[8][i]
+        J[x:y,x:y] = 1
+        if has_col_7_8:
+            if df[7][i]>=0: L[x] += df[6][i]*(1-df[7][i])
+            if df[8][i]>=0: L[y] += df[6][i]*(1-df[8][i])
+            if df[7][i]>=0: R[x] += df[6][i]*df[7][i]
+            if df[8][i]>=0: R[y] += df[6][i]*df[8][i]
+        else:
+            L[x] += df[6][i]
+            L[y] += df[6][i]
+            R[x] += df[6][i]
+            R[y] += df[6][i]
+
+    for i in range(N_beads-1):
+        J[i,i+1], J[i,i-1] = 1, 1
+        J[i+1,i], J[i-1,i] = 1, 1
     
     # Normalize (if neccesary): it means to convert values to probabilities
     if normalization:
@@ -60,9 +78,7 @@ def binding_vectors_from_bedpe(bedpe_file,N_beads,region,chrom,normalization=Fal
         axs[1].set_xlabel('Genomic Distance (with simumation beads as a unit)',fontsize=16)
         fig.show()
 
-    distances = np.array(distances)
-
-    return L, R, distances
+    return L, R, J
 
 def get_rnap_energy(path,region,chrom,N_beads,normalization):
     '''
@@ -119,6 +135,10 @@ def binding_matrix_from_bedpe(bedpe_file,N_beads,region,chrom,normalization=Fals
     plt.grid()
     plt.close()
 
+    print('Average loop size:',np.average(distances))
+    print('Median loop size:',np.median(distances))
+    print('Maximum loop size:', np.max(distances))
+
     return M, distances
 
 def hiccups_edit(file):
@@ -133,24 +153,24 @@ def hiccups_edit(file):
 def distance_point_line(x0,y0,a=1,b=-1,c=0):
     return np.abs(a*x0+b*y0+c)/np.sqrt(a**2+b**2)
 
-def load_track(file,region,chrom,N_beads,viz=False,roll=False):
-    bw = pyBigWig.open(file)
-    weights = bw_to_array(bw, region, chrom, N_beads,viz,roll)
-    return weights[:N_beads]
+# def load_track(file,region,chrom,N_beads,viz=False,roll=False):
+#     bw = pyBigWig.open(file)
+#     weights = bw_to_array(bw, region, chrom, N_beads,viz,roll)
+#     return weights[:N_beads]
 
-def bw_to_array(bw, region, chrom, N_beads, viz=False, roll=False):
-    step = (region[1]-region[0])//N_beads
-    bw_array = bw.values(chrom, region[0], region[1])
-    bw_array = np.nan_to_num(bw_array)
-    bw_array_new = list()
-    for i in range(step,len(bw_array)+1,step):
-        bw_array_new.append(np.average(bw_array[(i-step):i]))
-    weights = (np.roll(np.array(bw_array_new),3)+np.roll(np.array(bw_array_new),-3))/2 if roll else bw_array_new
-    if viz:
-        figure(figsize=(15, 5))
-        plt.plot(weights)
-        plt.grid()
-        plt.title('ChIP-Seq signal',fontsize=20)
-        plt.close()
+# def bw_to_array(bw, region, chrom, N_beads, viz=False, roll=False):
+#     step = (region[1]-region[0])//N_beads
+#     bw_array = bw.values(chrom, region[0], region[1])
+#     bw_array = np.nan_to_num(bw_array)
+#     bw_array_new = list()
+#     for i in range(step,len(bw_array)+1,step):
+#         bw_array_new.append(np.average(bw_array[(i-step):i]))
+#     weights = (np.roll(np.array(bw_array_new),3)+np.roll(np.array(bw_array_new),-3))/2 if roll else bw_array_new
+#     if viz:
+#         figure(figsize=(15, 5))
+#         plt.plot(weights)
+#         plt.grid()
+#         plt.title('ChIP-Seq signal',fontsize=20)
+#         plt.close()
     
-    return weights
+#     return weights
